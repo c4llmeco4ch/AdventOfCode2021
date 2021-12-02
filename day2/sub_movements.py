23768b9c3a04db2c7b0ca3ed1cd137e242f733b8
c4llmeco4ch <cbb692@gmail.com>
2021-12-02 15:28:31 -0500

    Added comment re walrus

--- day2/sub_movements.py
+++ day2/sub_movements.py
@@ -5,7 +5,7 @@ file_path = 'instructions.txt'
 x, y = 0, 0
 with open(file_path) as f:
     for line in f.readlines():
-        instr, amount = (s := line.split())[0], int(s[1])
+        instr, amount = (s := line.split())[0], int(s[1]) # :=, the walrus operator, will be discussed in another write-up
         if instr == 'forward':
             x += amount
         else:
@@ -18,7 +18,7 @@ print(f'Answer: {x * y}')
 x, y, aim = 0, 0, 0
 with open(file_path) as f:
     for line in f.readlines():
-        instr, amount = (s := line.split())[0], int(s[1])
+        instr, amount = (s := line.split())[0], int(s[1]) # see line 8
         if instr == 'forward':
             x += amount
             y += aim * amount
