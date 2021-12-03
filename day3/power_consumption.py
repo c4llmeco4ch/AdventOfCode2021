5ebcd68fab1aa5173c4eba03323b2e3a1b0d9298
c4llmeco4ch <cbb692@gmail.com>
2021-12-03 17:00:42 -0500

    Bout time, day 3 done

--- day3/power_consumption.py
+++ day3/power_consumption.py
@@ -12,3 +12,27 @@ epsilon = int(''.join([str(int(not num)) for num in gamma]), 2)
 gamma = int(''.join([str(i) for i in gamma]), 2)
 print(f'P1: Gamma = {gamma} | Epsilon = {epsilon}')
 print(f'Answer = {gamma * epsilon}')
+
+# Part 2
+def determine_split(my_list, pos):
+    totals = [0, 0]
+    for line in my_list:
+        totals[int(line[pos])] += 1
+    return totals
+
+most, least = list(lines), list(lines)
+for pm in range(line_length):
+    m0, m1 = determine_split(most, pm)
+    most = list(filter(lambda x: x[pm] == ('1' if m1 >= m0 else '0'), most))
+    if len(most) == 1:
+        oxy = int(most[0], 2)
+        break
+print("Least:")
+for pl in range(line_length):
+    l0, l1 = determine_split(least, pl)
+    least = list(filter(lambda x: x[pl] == ('0' if l1 >= l0 else '1'), least))
+    if len(least) == 1:
+        co2 = int(least[0], 2)
+        break
+print(f'P2: Oxy = {oxy} | CO2 = {co2}')
+print(f'Answer = {oxy * co2}')
