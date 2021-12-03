f8dea4f0fd50e3c6aa44f650891d79350ded2ea2
c4llmeco4ch <cbb692@gmail.com>
2021-12-03 02:18:52 -0500

    Fixed line 10 conditional

--- day3/power_consumption.py
+++ day3/power_consumption.py
@@ -7,7 +7,7 @@ for line in lines:
     one_count = [a + int(b) for a, b in zip(one_count, line.strip())]
 
 # Part 1
-gamma = [1 if val > len(lines) // 2 else 0 for val in one_count]
+gamma = [1 if val >= len(lines) // 2 else 0 for val in one_count]
 epsilon = int(''.join([str(int(not num)) for num in gamma]), 2)
 gamma = int(''.join([str(i) for i in gamma]), 2)
 print(f'P1: Gamma = {gamma} | Epsilon = {epsilon}')
