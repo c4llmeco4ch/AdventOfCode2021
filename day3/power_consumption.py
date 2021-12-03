12fe1a3f25edff50ca2769484c927baea26ab198
c4llmeco4ch <cbb692@gmail.com>
2021-12-03 02:13:20 -0500

    Part 1 done

---
+++ day3/power_consumption.py
@@ -0,0 +1,14 @@
+file_path = 'input.txt'
+with open(file_path) as f:
+    lines = f.readlines()
+line_length = len(lines[0].strip())
+one_count = [0] * line_length
+for line in lines:
+    one_count = [a + int(b) for a, b in zip(one_count, line.strip())]
+
+# Part 1
+gamma = [1 if val > len(lines) // 2 else 0 for val in one_count]
+epsilon = int(''.join([str(int(not num)) for num in gamma]), 2)
+gamma = int(''.join([str(i) for i in gamma]), 2)
+print(f'P1: Gamma = {gamma} | Epsilon = {epsilon}')
+print(f'Answer = {gamma * epsilon}')
